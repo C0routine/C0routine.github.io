8297d67432f302b3c342ab76d7bf6ab3a2f1ccef
Casan <whdrjsco@gmail.com>
2023-05-30 20:20:31 +0900

    fix/About Me 까지 Animation 적용

--- lib/view/screens/profile_screen.dart
+++ lib/view/screens/profile_screen.dart
@@ -1,8 +1,7 @@
-import 'package:blog/utils/app_color.dart';
+import 'package:flutter/material.dart';
+
 import 'package:blog/view/widgets/profile/about_me.dart';
 import 'package:blog/view/widgets/profile/intro.dart';
-import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -11,56 +10,58 @@ class ProfileScreen extends StatefulWidget {
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _ProfileScreenState extends State<ProfileScreen> with TickerProviderStateMixin {
+  final scrollController = ScrollController();
+
+  late AnimationController _aboutMeHeaderController;
+  late AnimationController _aboutMeBodyController;
+
+  void _checkScrollAnimation() {
+    print(scrollController.offset);
+    // AboutMe Header Animation Control
+    if (scrollController.offset > 40 && _aboutMeHeaderController.status == AnimationStatus.dismissed) {
+      _aboutMeHeaderController.forward();
+    }
+
+    // AboutMe Body Animation Control
+    if (scrollController.offset > 170 && _aboutMeBodyController.status == AnimationStatus.dismissed) {
+      _aboutMeBodyController.forward();
+    }
+  }
+
+  @override
+  void initState() {
+    // animation controller
+    _aboutMeHeaderController = AnimationController(vsync: this, duration: const Duration(milliseconds: 2000));
+    _aboutMeBodyController = AnimationController(vsync: this, duration: const Duration(milliseconds: 1500));
+    // scroll controller listener
+    scrollController.addListener(_checkScrollAnimation);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _aboutMeBodyController.dispose();
+    _aboutMeHeaderController.dispose();
+    scrollController.removeListener(_checkScrollAnimation);
+    scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // final pageController = PageController(initialPage: 0);
     return Stack(
       children: [
         ListView(
+          controller: scrollController,
           children: [
             ProfileIntro(),
-            ProfileAboutMe(),
+            ProfileAboutMe(
+              bodyController: _aboutMeBodyController,
+              headerController: _aboutMeHeaderController,
+            ),
           ],
         ),
-
-        // PageView(
-        //   controller: pageController,
-        //   scrollDirection: Axis.vertical,
-        //   physics: const PageScrollPhysics(),
-        //   pageSnapping: false,
-        //   children: [
-        //     ProfileIntro(),
-        //     ProfileAboutMe(),
-        //     // Center(child: Text('Third Page')),
-        //   ],
-        // ),
-        // Padding(
-        //   padding: const EdgeInsets.only(right: 20),
-        //   child: Align(
-        //     alignment: Alignment.centerRight,
-        //     child: SmoothPageIndicator(
-        //       count: 2,
-        //       effect: const SwapEffect(
-        //         type: SwapType.yRotation,
-        //         spacing: 12,
-        //         dotWidth: 16,
-        //         dotHeight: 10,
-        //         dotColor: AppColor.disable,
-        //         activeDotColor: AppColor.accent,
-        //       ),
-        //       controller: pageController,
-        //       axisDirection: Axis.vertical,
-        //       onDotClicked: (int page) {
-        //         pageController.animateToPage(
-        //           page,
-        //           curve: Curves.easeInOutBack,
-        //           duration: const Duration(milliseconds: 1200),
-        //         );
-        //       },
-        //     ),
-        //   ),
-        // ),
       ],
     );
   }
--- lib/view/widgets/profile/about_me.dart
+++ lib/view/widgets/profile/about_me.dart
@@ -1,51 +1,61 @@
-import 'package:blog/utils/app_color.dart';
-import 'package:blog/viewmodel/provider/responsive_vm.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
+import 'package:blog/utils/app_color.dart';
 
 class ProfileAboutMe extends StatefulWidget {
-  const ProfileAboutMe({Key? key}) : super(key: key);
+  const ProfileAboutMe({Key? key, required this.bodyController, required this.headerController}) : super(key: key);
+
+  final AnimationController bodyController;
+  final AnimationController headerController;
 
   @override
   State<ProfileAboutMe> createState() => _ProfileAboutMeState();
 }
 
 class _ProfileAboutMeState extends State<ProfileAboutMe> with TickerProviderStateMixin {
-  late AnimationController controller;
+  late Animation<double> _titleOpacity;
+  late Animation<double> _bodyScale;
 
   @override
   void initState() {
     super.initState();
+    _titleOpacity = Tween(begin: 0.0, end: 1.0)
+        .chain(
+          CurveTween(curve: Curves.decelerate),
+        )
+        .animate(widget.headerController);
 
-    controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+    _bodyScale = Tween(begin: 0.0, end: 1.0)
+        .chain(
+          CurveTween(curve: Curves.decelerate),
+        )
+        .animate(widget.bodyController);
   }
 
   @override
   Widget build(BuildContext context) {
-    final isDesktop = context.select((ResponsiveVM vm) => vm.extend);
-
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text('About Me', style: Theme.of(context).textTheme.titleLarge),
-        Container(
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: AppColor.backgroundSub,
-            borderRadius: BorderRadius.circular(16),
+        FadeTransition(
+          opacity: _titleOpacity,
+          child: Text('About Me', style: Theme.of(context).textTheme.titleLarge),
+        ),
+        ScaleTransition(
+          scale: _bodyScale,
+          child: Container(
+            margin: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: AppColor.backgroundSub,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Text(
+                '현재 Flutter 를 사용하여 프론트엔드 개발을 하고 있습니다.'
+                '\n사용자 경험(UX) 과 최적화 부분에 많은 관심을 가지고 있습니다.'
+                '\n장기적으로는 나만의 서비스를 만들고 운영하고 싶습니다!',
+                style: Theme.of(context).textTheme.bodyMedium),
           ),
-          child: Text(
-              '현재 Flutter 를 사용한 프론트엔드 개발을 하고 있습니다.'
-              '\n사용자 경험(UX) 과 최적화 부분에 많은 관심을 가지고 있습니다.'
-              '\n장기적으로는 나만의 서비스를 만들고 운영하고 싶습니다!',
-              style: Theme.of(context).textTheme.bodyMedium),
         ),
       ],
     );
